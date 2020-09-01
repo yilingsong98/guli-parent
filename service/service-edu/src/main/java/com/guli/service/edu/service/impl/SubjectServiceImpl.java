53f5dfa0f9d09e428cf12150770945b71194de4d
donkey_zwx <donkey98@163.com>
2020-09-01 18:32:13 +0800

    添加事务注解

--- service/service-edu/src/main/java/com/guli/service/edu/service/impl/SubjectServiceImpl.java
+++ service/service-edu/src/main/java/com/guli/service/edu/service/impl/SubjectServiceImpl.java
@@ -1,10 +1,19 @@
 package com.guli.service.edu.service.impl;
 
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.support.ExcelTypeEnum;
 import com.guli.service.edu.entity.Subject;
+import com.guli.service.edu.entity.excel.ExcelSubjectData;
+import com.guli.service.edu.entity.excel.ExcelSubjectDataListener;
+import com.guli.service.edu.entity.vo.SubjectVo;
 import com.guli.service.edu.mapper.SubjectMapper;
 import com.guli.service.edu.service.SubjectService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.InputStream;
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +26,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class SubjectServiceImpl extends ServiceImpl<SubjectMapper, Subject> implements SubjectService {
 
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public void batchImport(InputStream inputStream) {
+        EasyExcel.read(inputStream, ExcelSubjectData.class,
+                new ExcelSubjectDataListener(baseMapper)).excelType(ExcelTypeEnum.XLS).sheet().doRead();
+
+    }
+
+    @Override
+    public List<SubjectVo> nestedList() {
+        return null;
+    }
 }
