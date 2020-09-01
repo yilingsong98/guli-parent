4f8f31777440961608b9bfd1bb040762652d52fd
donkey_zwx <donkey98@163.com>
2020-09-01 18:08:28 +0800

    excel文件上传模块实体类

---
+++ service/service-edu/src/main/java/com/guli/service/edu/entity/excel/ExcelSubjectData.java
@@ -0,0 +1,14 @@
+package com.guli.service.edu.entity.excel;
+
+import com.alibaba.excel.annotation.ExcelProperty;
+import lombok.Data;
+
+@Data
+public class ExcelSubjectData {
+
+    @ExcelProperty("一级分类")
+    private String levelOneTitle;
+
+    @ExcelProperty("二级分类")
+    private String levelTwoTitle;
+}
